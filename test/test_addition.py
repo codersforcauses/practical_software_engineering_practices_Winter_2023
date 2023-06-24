cea15cf3c097f0bb0f678a93c5257f0d4a2aa0a9
ellenxiao0412@gmail.com <ellenxiao0412@gmail.com>
2023-06-24 14:56:11 +0800

    Added factorial function.

--- test/test_addition.py
+++ test/test_addition.py
@@ -7,3 +7,17 @@ def test_add():
     assert add(1, 1) == 2
     assert add(0, 0) == 0
     assert add(1, -1) == 0
+
+
+def factorial(n):
+    """Return n!"""
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+def test_factorial():
+    assert factorial(1) == 1
+    assert factorial(2) == 2
+    assert factorial(3) == 6
