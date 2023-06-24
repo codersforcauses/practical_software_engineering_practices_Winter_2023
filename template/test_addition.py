eb803384a0f423d2a590e3be7dfe686cdee36126
lilylyi <lilyy395@gmail.com>
2023-06-24 15:08:24 +0800

    add divide function

--- template/test_addition.py
+++ template/test_addition.py
@@ -7,3 +7,11 @@ def test_add():
     assert add(1, 1) == 2
     assert add(0, 0) == 0
     assert add(1, -1) == 0
+
+def divide(a,b):
+    return a/b
+
+def test_divide(a,b):
+    assert divide(2,2) == 1
+    assert divide(4,2) == 2
+    assert divide(6,2) == 3
