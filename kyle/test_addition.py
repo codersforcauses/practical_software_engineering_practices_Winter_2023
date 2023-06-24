4b96d5018d8fc1523252d181730453920b1bc4ee
Kyle Leung <lkkc0300@gmail.com>
2023-06-25 00:33:21 +0800

    i30-test

--- kyle/test_addition.py
+++ kyle/test_addition.py
@@ -2,10 +2,8 @@ def add(a, b):
     """Add a and b"""
     return a + b
 
-
 def test_add():
     assert add(1, 1) == 2
     assert add(0, 0) == 0
     assert add(1, -1) == 0
-
-print("yo")
+    assert add(3, 4) == 7
